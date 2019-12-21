f7abb88716f641aca6d6c4e60ef6ae4700978ecd
Hannah Harrington <hannahharrington@outlook.com>
2019-12-21 13:17:36 +0000

    Done.

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -1,12 +1,28 @@
-require 'pry'
+list = ["ab", "cd", "ef"]
 
-def hello(array)
+def my_each(array)
   i = 0
   while i < array.length
+# The "while" method will always return a value of => nil"
     yield(array[i])
-    i += 1
+    i = i + 1
   end
 end
 
+my_each(list) {|i| puts "So I think #{i} is a piece of shit"}
 
-hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
+# Use the collection method 
+# To make the *return-value* the answer you want
+# instead of "=> nil"
+
+def hello(array)
+  i = 0
+  collection = []
+  while i < array.length
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
+end
+
+hello(list) {|i| "Hello #{i} what's up?"}
